dfe1bef8e8aabf83fa0578d1475796d16217e4b4
Paul Duvall <paul.duvall@myfuturechoice.com>
2023-01-14 10:35:18 +0000

    feat: add basic vuetify layout

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,31 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
+  <v-app>
+    <v-app-bar color="blue-lighten-2">
+      <v-app-bar-title>Boardgame picker</v-app-bar-title>
+    </v-app-bar>
+    <v-main>
+      <RouterView />
+    </v-main>
+    <v-footer class="bg-blue-lighten-1">
+      <v-row justify="center" no-gutters>
+        <v-col class="text-center mt-4" cols="12">
+        {{ new Date().getFullYear() }} — <strong>Paul Duvall</strong>
+      </v-col>
+      </v-row>
+    </v-footer>
+  </v-app>
+  <!-- <header>
     <div>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
-  </header>
+  </header> -->
 
-  <RouterView />
+
 </template>
 
 <style scoped>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,20 @@
 </script>
 
 <template>
-  <main class="text-uppercase">
-    hi
-  </main>
+  <v-card width="400" class="mt-5 ml-5">
+      <v-card-item>
+        <v-card-title>Welcome to the Game Picker!</v-card-title>
+
+        <v-card-subtitle>Want to play a game but can't decide which one?</v-card-subtitle>
+      </v-card-item>
+
+      <v-card-text>
+      Enter your BGG username and you can randomly select a game to play from your collection.
+      No idea what any of this nonsense is about? Click for help.
+      </v-card-text>
+    </v-card>
+
+    <!-- 1 - set up a simple landing page
+    2 - get pinia working
+    3 - pull in data from bgg and save to pinia store -->
 </template>
