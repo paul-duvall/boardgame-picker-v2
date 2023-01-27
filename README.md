e2be679d95fded7b08600ae446aaa882a9c19a80
Paul Duvall <paul.duvall@myfuturechoice.com>
2023-01-27 17:18:44 +0000

    chore: add axios and initial Pinia setup

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # boardgame-picker-v2
+
+Built using the following:
+
+- Vue 3
+- Vuetify
+- Vue Router
+
+Will also utilise:
+
+- Pinia
+- Vitest
+- Typescript
  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
+import { ref, reactive } from 'vue'
+import { useAppStore } from '@/store/app';
+import { storeToRefs } from 'pinia';
+
+const appStore = useAppStore();
+
+const { test } = storeToRefs(appStore);
 </script>
 
 <template>
   <v-card width="400" class="mt-5 ml-5">
       <v-card-item>
+        {{ test }}
         <v-card-title>Welcome to the Game Picker!</v-card-title>
-
         <v-card-subtitle>Want to play a game but can't decide which one?</v-card-subtitle>
       </v-card-item>
 
